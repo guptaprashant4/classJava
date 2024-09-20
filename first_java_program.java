9697e9cba0960e259813c5578b715413874c3f59
Prashant Gupta <93766389+guptaprashant4@users.noreply.github.com>
2024-09-20 16:05:35 -0500

    Add files via upload
    This was the first java program I wrote in class.

---
+++ first_java_program.java
@@ -0,0 +1,61 @@
+/*
+ * Purpose: Practice working with the IDE and native datatypes
+ * Author: Prashant Gupta
+ * Date modified: 18 Aug 2023
+ */
+
+public class M1_Lab1 {
+
+    public static void main(String[] args) {
+
+        System.out.print("\n\nExercise 1");
+        System.out.print("\n\nHello World\n\n");
+
+        String firstName = "Prashant";
+        String lastName = "Gupta";
+
+        System.out.print("Hello, " + firstName + " " + lastName + "\n\n");
+
+        int factor = 13;
+        double rate = 4.5;
+        char response = 'y';
+
+        System.out.print(factor + " is a factor of 39\n");
+        System.out.println("The interest rate on my house is " + rate);
+        System.out.println("Your response was '" + response + "'");
+        System.out.println("Their response was \"Not in this lifetime\"");
+
+
+        System.out.println("\n\nExercise 2\n");
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Major: Computer Engineering");
+        System.out.println("Class: Sophomore");
+
+        System.out.println("\n\nExercise 3\n");
+        
+        System.out.print("\t         (      )\n");
+        System.out.print("                 ~(^^^^)~\n");
+        System.out.print("                  ) @@ \\~_          |\\\n");
+        System.out.print("                 /     | \\        \\\" /\n");
+        System.out.print("                ( 0  0  ) \\        | | \n");
+        System.out.print("                 ---___/~  \\       | |\n");
+        System.out.print("                  /'__/ |   ~-_____/ |\n");
+        System.out.print("   o          _   ~----~      ___---~\n");
+        System.out.print("     O       //     |         |\n");
+        System.out.print("            ((~\\  _|        -|\n");
+        System.out.print("      o  O //-_ \\/ |        ~  |\n");
+        System.out.print("           ^   \\_ /         ~  |\n");
+        System.out.print("                  |          ~ |\n");
+        System.out.print("                  |     /     ~ |\n");
+        System.out.print("                  |     (       |\n");
+        System.out.print("                   \\     \\      /\\\n");
+        System.out.print("                  / -_____-\\   \\ ~~-*\n");
+        System.out.print("                  |  /       \\  \\  \n");
+        System.out.print("                  / /         / / \n");
+        System.out.print("                /~  |      //~  |\n");
+        System.out.print("                ~~~~        ~~~~\n");
+
+        System.out.print("\n                    Cow\n\n");
+
+    }
+}
